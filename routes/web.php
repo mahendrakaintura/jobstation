ea78026020cfed0e6030e1433d9c098a2100c8ee
Osawa <175920342+jobstationabs@users.noreply.github.com>
2024-11-01 14:23:31 +0900

    route修正

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//管理画面
+//ログイン画面
+Route::get('/login/_sales', fn () => '営業ログイン画面')->name('sales.login');		//営業
+Route::get('/login/_admin', fn () => '管理者ログイン画面')->name('admin.login');	//管理者
+
+
+//営業
+Route::namespace('sales')
+	->name('sales.')
+	->prefix('sales')
+	//->middleware('')
+	->group(function() {
+
+	});
+
+//管理者
 Route::namespace('admin')
 	->name('admin.')
 	->prefix('admin')
 	//->middleware('')
 	->group(function() {
 
-		Route::get('/', fn () => 'adminログイン画面')->name('login');	//ログイン画面
-
+		
 		//メール送信テスト用
-		Route::get('mail', function () {
-			Mail::raw('test mail',function ($message) {$message->to('test@to.com')->subject('test subject');});
-			return 'テストメール送信完了。';
-		});
-
-		//マッチしなかったURLは全てログイン画面へリダイレクト
-		Route::any('{all}', fn () => Redirect::route('admin.login')->setStatusCode(RedirectResponse::HTTP_SEE_OTHER));
+		// Route::get('mail', function () {
+		// 	Mail::raw('test mail',function ($message) {$message->to('test@to.com')->subject('test subject');});
+		// 	return 'テストメール送信完了。';
+		// });
 
 	});
+
+//マッチしなかったURLはトップ画面へリダイレクト
+Route::any('{all}', fn () => Redirect::to('/', RedirectResponse::HTTP_SEE_OTHER));
