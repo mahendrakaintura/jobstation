134ab802988ab34fad4127ccaf82f7ebfdfafdfd
Sato <175920342+jobstationabs@users.noreply.github.com>
2024-12-08 22:07:58 -0500

    feat: 応募履歴画面を実装

--- resources/js/Pages/Mypage/Entry.vue
+++ resources/js/Pages/Mypage/Entry.vue
@@ -1,12 +1,22 @@
 <script setup>
+import { Head, usePage } from '@inertiajs/vue3'
+import { ref, watch, onMounted } from 'vue'
+import { router } from '@inertiajs/vue3'
 import AppLayout from '@/Layouts/AppLayout.vue'
+import EntryCard from '@/Components/Entry/EntryCard.vue';
 
 const props = defineProps({
-    //
+    entries: {
+        type: Object,
+        required: true
+    }
 })
+
+
 </script>
 
 <template>
+    <Head title="応募履歴" />
     <AppLayout>
         <template #main>
             <div class="py-12">
@@ -14,6 +24,9 @@ const props = defineProps({
                     <div class="bg-white p-6 shadow rounded-lg">
                         <h2 class="text-lg font-medium mb-6">応募履歴</h2>
                         <!-- // -->
+                        <div v-for="entry in entries.data" :key="entry.id" class="flex flex-col">
+                            <EntryCard :entry="entry" />
+                        </div>
                     </div>
                 </div>
             </div>
